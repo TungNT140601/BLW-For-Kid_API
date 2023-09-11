2171d52d33fbb3c2f6b1ffa845871673775c7c6f
TungNT140601 <tungnt14062001@gmail.com>
2023-09-11 21:40:13 +0700

    add column to paymentHistories table

--- Repositories/EntityModels/PaymentHistory.cs
+++ Repositories/EntityModels/PaymentHistory.cs
@@ -5,20 +5,20 @@ namespace Repositories.EntityModels
 {
     public partial class PaymentHistory
     {
-        public string PaymentId { get; set; } = null!; //vnp_TxnRef
+        public string PaymentId { get; set; } = null!;
         public string CustomerId { get; set; } = null!;
-        public double Amount { get; set; }
+        public string? Amount { get; set; }
         public string CreateDateS { get; set; } = null!;
-        public DateTime? CreateDate { get; set; }
+        public DateTime CreateDate { get; set; }
         public string IpAddr { get; set; } = null!;
         public string OrderInfo { get; set; } = null!;
         public string TxnRef { get; set; } = null!;
-        public string ResponseCode { get; set; } = null;
-        public string TransactionNo { get; set; } = null;
+        public string? ResponseCode { get; set; }
+        public string? TransactionNo { get; set; }
         public DateTime? PurchaseTime { get; set; }
         public int? NumOfMonth { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
 
         public virtual CustomerAccount Customer { get; set; } = null!;
     }
