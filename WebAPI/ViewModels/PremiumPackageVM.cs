74df1c85b3be32ec9d6a80b409c4e4da83e1ebcc
truong2451 <huutruong2451@gmail.com>
2023-09-14 21:46:02 +0700

    add function table premiumpackage

---
+++ WebAPI/ViewModels/PremiumPackageVM.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.ViewModels
+{
+    public class PremiumPackageVM
+    {
+        public string PackageName { get; set; }
+        public decimal PackageAmount { get; set; }
+        public double? PackageDiscount { get; set; }
+        public int PackageMonth { get; set; }
+    }
+}
