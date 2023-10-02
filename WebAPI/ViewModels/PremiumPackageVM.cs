5a212cd4909faf9c1bf8b2dedf3a6e503080c4ce
truong2451 <huutruong2451@gmail.com>
2023-10-02 16:28:36 +0700

    check

--- WebAPI/ViewModels/PremiumPackageVM.cs
+++ WebAPI/ViewModels/PremiumPackageVM.cs
@@ -1,6 +1,20 @@
-﻿namespace WebAPI.ViewModels
+﻿using Repositories.EntityModels;
+using System.Text.Json.Serialization;
+
+namespace WebAPI.ViewModels
 {
     public class PremiumPackageVM
+    {
+        public string PackageId { get; set; } = null!;
+        public string PackageName { get; set; } = null!;
+        public decimal PackageAmount { get; set; }
+        public double? PackageDiscount { get; set; }
+        public int PackageMonth { get; set; }
+        public bool IsDelete { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<PaymentHistory> PaymentHistories { get; set; }
+    }
+    public class PremiumPackageAddVM
     {
         public string PackageName { get; set; }
         public decimal PackageAmount { get; set; }
