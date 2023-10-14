17755526d3f3a6a49a15fa2f5264231441483511
Caotuongg <tuongdcse151377@fpt.edu.vn>
2023-10-14 21:08:52 +0700

    Fix Ingredient

--- WebAPI/ViewModels/IngredientVM.cs
+++ WebAPI/ViewModels/IngredientVM.cs
@@ -1,21 +1,39 @@
-﻿namespace WebAPI.ViewModels
+﻿using System.Text.Json.Serialization;
+
+namespace WebAPI.ViewModels
 {
     public class IngredientVM
     {
-        public string? IngredientId { get; set; }
+        
         public string? IngredientName { get; set; }
-        public string? IngredientImage { get; set; }
         public string? Measure { get; set; }
         public double? Protein { get; set; }
         public double? Carbohydrate { get; set; }
         public double? Fat { get; set; }
         public double? Calories { get; set; }
-        public DateTime? CreateTime { get; set; }
-        public string? StaffCreate { get; set; }
-        public DateTime? UpdateTime { get; set; }
-        public string? StaffUpdate { get; set; }
-        public DateTime? DeleteDate { get; set; }
-        public string? StaffDelete { get; set; }
-        public bool? IsDelete { get; set; }
+        [JsonIgnore]
+        public string? StaffCreate { get; set; } 
+
     }
+
+    public class IngredientUpdateVM
+    {
+        public string? IngredientId { get; set; } = null!;
+        public string? IngredientName { get; set; }
+        public string? Measure { get; set; }
+        public double? Protein { get; set; }
+        public double? Carbohydrate { get; set; }
+        public double? Fat { get; set; }
+        public double? Calories { get; set; }
+        [JsonIgnore]
+        public string? StaffUpdate { get; set; }
+    }
+     public class IngredientDeleteVM
+    {
+        public string? IngredientId { get; set; }
+        [JsonIgnore]
+        public string? StaffDelete { get; set; }
+    }
+
+
 }
